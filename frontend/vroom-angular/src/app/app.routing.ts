aa1941ef81a6f6908cf9288d65ac3c0ee47b18ff
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2025-03-30 21:33:37 +0200

    front project update + remove old ones

---
+++ frontend/vroom-angular/src/app/app.routing.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './examples/login/login.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: 'index',                component: ComponentsComponent },
+    { path: 'login',                component: LoginComponent },
+    { path: 'vehicles',             component:LandingComponent },
+    { path: 'vehicles/:id',         component:LandingComponent },
+    { path: 'profile',               component: ProfileComponent },
+    { path: 'your-reservations',    component: ProfileComponent },
+    // { path: 'examples/profile',     component: ProfileComponent },
+    // { path: 'nucleoicons',          component: NucleoiconsComponent },
+    // { path: 'examples/landing',     component: LandingComponent },
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
